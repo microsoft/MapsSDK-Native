030046c9e5912ab62e020ff0bc386c9266137c11
khass891891 <khass@microsoft.com>
2019-10-09 16:48:19 -0700

    Update SDK and search samples for Android

--- Android/samples/search/app/src/main/java/com/microsoft/maps/sample/search/MainActivity.java
+++ Android/samples/search/app/src/main/java/com/microsoft/maps/sample/search/MainActivity.java
@@ -57,8 +57,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMapView = new MapView(this, MapRenderMode.VECTOR);
+        mMapView.onCreate(savedInstanceState);
         mMapView.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
         ((FrameLayout)findViewById(R.id.map_view)).addView(mMapView);
+        mMapView.setScene(
+                MapScene.createFromLocationAndZoomLevel(LOCATION_LAKE_WASHINGTON, 10),
+                MapAnimationKind.NONE);
 
         MapServices.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
 
@@ -131,9 +135,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        mMapView.setScene(
-                MapScene.createFromLocationAndZoomLevel(LOCATION_LAKE_WASHINGTON, 10),
-                MapAnimationKind.NONE);
+        if (mMapView != null) {
+            mMapView.onStart();
+        }
     }
 
     @Override
@@ -149,6 +153,22 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         if (mMapView != null) {
             mMapView.onPause();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mMapView != null) {
+            mMapView.onSaveInstanceState(outState);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mMapView != null) {
+            mMapView.onStop();
         }
     }
 
